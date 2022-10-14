1d3122944b1bd4f5cf53af1b71dc1c52823d8fe7
Moeen Mahmud <write2fahim007@gmail.com>
2022-10-14 19:29:54 +0600

    bootstraped the bee-commerce project

---
+++ apps/bee-commerce/jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'bee-commerce',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/bee-commerce',
+};
---
+++ apps/bee-commerce/src/app/app.js
@@ -0,0 +1,28 @@
+import { Route, Routes, Link } from 'react-router-dom';
+export function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+    </>
+  );
+}
+export default App;
